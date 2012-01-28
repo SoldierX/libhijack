b9599475045de23beddb87ae6cddc78d4d17438e
Shawn Webb <lattera@gmail.com>
2012-01-28 15:54:09 -0700

    Bump version number to 0.6

--- src/libhijack.c
+++ src/libhijack.c
@@ -85,7 +85,7 @@ EXPORTED_SYM HIJACK *InitHijack(void)
 	
 	memset(hijack, 0x00, sizeof(HIJACK));
 	
-	hijack->version = "0.5.2";
+	hijack->version = "0.6";
 	
 	SetValue(hijack, V_BASEADDR, &baseaddr);
 	
