5733ce7fb043dfee22a900ccf0790932f8e52a8f
Shawn Webb <lattera@gmail.com>
2012-03-15 09:13:49 -0600

    Add start of RTLD code

---
+++ freebsd_tests64/testso.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func(void)
+{
+    printf("blah\n");
+    return 0;
+}
--- src/rtld.c
+++ src/rtld.c
@@ -8,6 +8,7 @@
 #include <sys/user.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <sys/param.h>
 
 #include <elf.h>
 #include <link.h>
@@ -26,7 +27,10 @@ struct rtld_loadable {
         ElfW(Phdr) *phdr;
     } phdr;
 
-    unsigned long mapping;
+    unsigned long vaddr;
+    unsigned long addr;
+    unsigned long limit;
+    unsigned long offset;
 
     struct rtld_loadable *next;
 };
@@ -48,6 +52,13 @@ struct rtld_aux {
         unsigned char *buf;
         ElfW(Phdr) *phdr;
     } phdr;
+
+    unsigned long base_addr;
+    unsigned long base_vaddr;
+    unsigned long base_offset;
+    unsigned long base_vlimit;
+    unsigned long mapsize;
+    unsigned long mapping;
 
     struct rtld_loadable *loadables;
 };
@@ -92,11 +103,30 @@ int rtld_load_headers(HIJACK *hijack, struct rtld_aux *aux) {
     return 0;
 }
 
+/*
+ * Actually load the shared object
+ * Logic taken from freebsd/9-stable/libexec/rtld-elf/map_object.c
+ */
 void rtld_create_maps(HIJACK *hijack, struct rtld_aux *aux) {
-    struct rtld_loadable *loadable;
+    struct rtld_loadable *first_loadable, *last_loadable;
 
-    for (loadable = aux->loadables; loadable != NULL; loadable = loadable->next) {
-        loadable->mapping = MapMemory(hijack, (unsigned long)NULL, loadable->phdr.phdr->p_memsz, loadable->phdr.phdr->p_flags, MAP_ANONYMOUS|MAP_SHARED);
+    /* Grab first and last loadable PHDRs */
+    first_loadable = aux->loadables;
+    for (last_loadable = aux->loadables; last_loadable->next != NULL; last_loadable = last_loadable->next)
+        ;
+
+    aux->base_offset = trunc_page(first_loadable->phdr.phdr->p_offset);
+    aux->base_vaddr = trunc_page(first_loadable->phdr.phdr->p_vaddr);
+    aux->base_vlimit = round_page(last_loadable->phdr.phdr->p_vaddr + last_loadable->phdr.phdr->p_memsz);
+    aux->mapsize = aux->base_vlimit - aux->base_vaddr;
+    aux->base_addr = (unsigned long)NULL;
+
+    aux->mapping = MapMemory(hijack, aux->base_addr, aux->mapsize, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_SHARED);
+
+    if (IsFlagSet(hijack, F_DEBUG)) {
+        fprintf(stderr, "map[0x%016lx]:\n", aux->mapping);
+        fprintf(stderr, "    mapsize\t= %lu\n", aux->mapsize);
+        fprintf(stderr, "    limit\t= %lu\n", aux->base_vlimit);
     }
 }
 
